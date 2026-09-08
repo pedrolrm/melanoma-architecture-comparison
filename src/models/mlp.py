1d28cb3e23f82e607c52ec1e2bb19de305a9fb6b
pedrolrm <p.lucarocha@gmail.com>
2026-09-08 18:24:04 -0300

    chore: mudanças de funções na mlp

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,50 +1,52 @@
 import torch
 import torch.nn as nn
 
-class MelanomaMLP(nn.Module):
+class MLP(nn.Module):
     def __init__(self):
-        super(MelanomaMLP, self).__init__()
-        # Camada 1 
-        self.fc1 = nn.Linear(in_features=224 * 224 * 3, out_features=512)
-        self.bn1 = nn.BatchNorm1d(512)
+        super().__init__()
+        self.flatten = nn.Flatten()
+        
+        # Camada 1
+        self.fc1 = nn.Linear(3 * 224 * 224, 1024)
+        self.bn1 = nn.BatchNorm1d(1024)
         self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(p=0.4)
+        self.drop1 = nn.Dropout(0.5)
 
         # Camada 2
-        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.fc2 = nn.Linear(1024, 256)
         self.bn2 = nn.BatchNorm1d(256)
         self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(p=0.3)
+        self.drop2 = nn.Dropout(0.5)
 
-        # Camada 3 
-        self.fc3 = nn.Linear(in_features=256, out_features=64)
+        # Camada 3
+        self.fc3 = nn.Linear(256, 64)
         self.bn3 = nn.BatchNorm1d(64)
         self.relu3 = nn.ReLU()
-        self.dropout3 = nn.Dropout(p=0.2)
+        self.drop3 = nn.Dropout(0.3)
 
-        # Camada de saída
-        self.fc_out = nn.Linear(in_features=64, out_features=1)
+        # Camada de saída com Sigmoid
+        self.classifier = nn.Linear(64, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
-
+        x = self.flatten(x)
+        
         x = self.fc1(x)
         x = self.bn1(x)
         x = self.relu1(x)
-        x = self.dropout1(x)
+        x = self.drop1(x)
 
         x = self.fc2(x)
         x = self.bn2(x)
         x = self.relu2(x)
-        x = self.dropout2(x)
+        x = self.drop2(x)
 
         x = self.fc3(x)
         x = self.bn3(x)
         x = self.relu3(x)
-        x = self.dropout3(x)
+        x = self.drop3(x)
 
-        x = self.fc_out(x)
-        x = self.sigmoid(x)
-
+        x = self.classifier(x)
+        x = self.sigmoid(x)  # Retorna probabilidades entre 0 e 1
+        
         return x
